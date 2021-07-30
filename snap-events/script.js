e021a9f5aef5433a88c57520b3b97d913ea75818
Andrew Powers <apowers10@cnm.edu>
2021-07-30 11:28:38 -0400

    first final draft

---
+++ snap-events/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("theButton");
+button.style.backgroundColor = "#FFFFFF";
+
+function buttonClick() {
+    const button = document.getElementById("theButton");
+
+    button.style.backgroundColor = "red";
+}
